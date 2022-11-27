9f65f20fee29668ad0fdb71336250386452910f2
siddhant <siddhantdeshpande3@gmail.com>
2022-11-27 05:50:31 +0100

    implemented simple gserve and gproxy

--- grproxy/src/grproxy/grproxy.go
+++ grproxy/src/grproxy/grproxy.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/samuel/go-zookeeper/zk"
+)
+
 func main() {
+
+	conn, _, err := zk.Connect([]string{"zookeeper"}, time.Second) //*10)
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(5 * time.Second)
+	serverList(conn)
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func serverList(conn *zk.Conn) {
+	fmt.Println("checking...")
+
+	children, _, _, err := conn.ChildrenW("/servers")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, child := range children {
+		fmt.Println(child)
+	}
 
 }
--- gserve/src/gserve/gserve.go
+++ gserve/src/gserve/gserve.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"time"
 
+	"github.com/samuel/go-zookeeper/zk"
+)
+
+func main() {
+	fmt.Println(os.Getenv("NAME"), "container started!")
+
+	conn, _, err := zk.Connect([]string{"zookeeper"}, time.Second) //*10)
+	if err != nil {
+		panic(err)
+	}
+
+	children, _, _, err := conn.ChildrenW("/")
+	if err != nil {
+		panic(err)
+	}
+
+	if !contains(children, "servers") {
+		fmt.Println("Creating servers path...")
+		serverPath := "/servers"
+		if p, err := conn.Create(serverPath, nil, 0, zk.WorldACL(zk.PermAll)); err != nil {
+			panic(err)
+		} else if p != serverPath {
+			fmt.Printf("Create returned different path '%s' != '%s'", p, serverPath)
+		} else {
+			createNode(conn)
+		}
+	} else {
+		createNode(conn)
+	}
+
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func createNode(conn *zk.Conn) {
+	path := "/servers/" + os.Getenv("NAME")
+
+	if p, err := conn.Create(path, nil, 0, zk.WorldACL(zk.PermAll)); err != nil {
+		panic(err)
+	} else if p != path {
+		fmt.Printf("Create returned different path '%s' != '%s'", p, path)
+	}
 }
